0a98288615fc10293aed3ae7fb00726fef2f102d
Dan G <daniel.w.gray1994@gmail.com>
2023-07-28 13:22:07 -0400

    added a resolver to prompt chat gpt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,26 @@
 const { User } = require("../models");
-const { signToken, AuthenticationError } = require("../utils/auth");
+const { signToken, AuthenticationError, openAI } = require("../utils");
 
 const resolvers = {
   Mutation: {
-    addQuestion: async (parent, args) => {},
+    addQuestion: async (parent, args) => {
+      const prompt = await openAI.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an interviewer for a Data Analytics company for a Fortune 500 company.",
+          },
+          {
+            role: "user",
+            content: "What is one question you have for me for a candidate?",
+          },
+        ],
+      });
+
+      return prompt;
+    },
   },
 };
 
