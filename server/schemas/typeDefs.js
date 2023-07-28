21519d33be03f2e1f9118dedb3601e6f546d76b9
Dan G <daniel.w.gray1994@gmail.com>
2023-07-28 13:33:05 -0400

    added a typeDefs export

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,15 @@
-const typeDefs = ``;
+const typeDefs = `
+type Question {
+    question: String!
+}
+
+type Query {
+    dummyQuery: String
+}
+
+type Mutation {
+    addQuestion(question: String!): Question
+}
+`;
+
+module.exports = typeDefs;
