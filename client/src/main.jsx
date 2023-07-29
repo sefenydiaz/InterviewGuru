be59eb40d0976bb68dfce28a97360a82a7a9dbf7
Lauren Miller <lmiller90@fordham.edu>
2023-07-29 15:03:00 -0400

    reworked main and app jsx to see application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,32 +1,7 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import App from './App.jsx';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }, {
-        path: '/matchup',
-        element: <Matchup />
-      }, {
-        path: '/matchup/:id',
-        element: <Vote />
-      },
-    ],
-  },
-]);
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App />
 );
