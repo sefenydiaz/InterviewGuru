7f32b1bebefbf9d8f182aa8000f6b0af761d0d0c
Dan G <daniel.w.gray1994@gmail.com>
2023-07-29 12:52:54 -0400

    added an answer property to the question model

--- server/models/Question.js
+++ server/models/Question.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+const Answer = require("./Answer");
 
 const questionSchema = new Schema({
   question: {
     type: String,
     required: true,
   },
+  answer: Answer.schema,
 });
 
 const Question = mongoose.model("Question", questionSchema);
