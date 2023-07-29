095fc5226fbb9677b352165e633cff7ee15f2cd0
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-07-29 18:49:30 +0200

    added starter code to AI and interview

--- client/src/pages/Interview.jsx
+++ client/src/pages/Interview.jsx
@@ -9,21 +9,25 @@ export const quiz =  {
     {
       "question": "How can you access the state of a component from inside of a member function?",
       "questionType": "text",
-      "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
+      // "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
       "answerSelectionType": "input",
-      "answers": [
-      //   "this.getState()",
-      //   "this.prototype.stateValue",
-      //   "this.state",
-      //   "this.values"
-      ],
+      // "answers": [
+      // //   "this.getState()",
+      // //   "this.prototype.stateValue",
+      // //   "this.state",
+      // //   "this.values"
+      // ],
       "correctAnswer": "3",
       "messageForCorrectAnswer": "Correct answer. Good job.",
       "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      // "point": "20"
     }
   ]};
+
+
+
+
 
 // import { useQuery, useMutation } from '@apollo/client';
 // import { useParams, Link } from 'react-router-dom';
