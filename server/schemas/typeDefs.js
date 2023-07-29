f2f3bd65ed6cc63f7108209b293cea8346c62a76
Tawhid <tawhidkamal5@gmail.com>
2023-07-29 11:50:58 -0400

    added User query and auth query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,20 @@
 const typeDefs = `
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+}
+
 type Question {
     _id: ID!
     question: String!
 }
+
+type Auth {
+    token: ID
+    user: User
+  }
 
 type Query {
     questions: [Question]
