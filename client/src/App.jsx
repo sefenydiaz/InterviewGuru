565e4b1e9e600122bf2d1ce8f0e20907a8d17d5b
Lauren Miller <lmiller90@fordham.edu>
2023-07-31 10:46:50 -0400

    fixed app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css';
 //import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { ChakraProvider } from '@chakra-ui/react'; 
-import { Home } from './pages/Home'
-import { Footer } from './components/Footer'
 
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ChakraProvider } from '@chakra-ui/react'; 
+import Header  from './components/Header/index'
+import Footer from './components/Footer/index'
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -15,8 +36,11 @@ function App() {
   return (
       <ChakraProvider >
         <ApolloProvider client={client}>
-          <div className="guru">
-            <Home />
+          <div className="container">
+            <Header />
+            <div className="container">
+            <Outlet />
+            </div>
             <Footer />
           </div>
         </ApolloProvider>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,5 @@
-import { useState } from 'react';
-import Header from '../components/Header/index';
-import Welcome from './Welcome'
-import Form from './Form';
-import Questions from './Questions';
-import Stats from './Stats';
-
-
-export default function Home() {
-  const [currentPage, setCurrentPage] = useState('Form');
-  const renderPage = () => {
-    if (currentPage === 'Welcome') {
-      return <Welcome />;
-    }
-    if (currentPage === 'Form') {
-      return <Form />;
-    }
-    if (currentPage === 'Questions') {
-      return <Questions />;
-    }
-    return <Stats />;
-  };
-const handlePageChange = (page) => setCurrentPage(page);
-
-return (
-  <div>
-    {/* We are passing the currentPage from state and the function to update it */}
-    <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-    {/* Here we are calling the renderPage method which will return a component  */}
-    <main className="mx-3">{renderPage()}</main>
-  </div>
-);
-
+const Home = () => {
+    
 }
+
+export default Home;
