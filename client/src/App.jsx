f726874dc9f66f09f3e55d6c76f1b3796a205f8f
Lauren Miller <lmiller90@fordham.edu>
2023-07-31 08:58:39 -0400

    removed double heading render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 //import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react'; 
-import { Header } from './components/Header/index'
 import { Home } from './pages/Home'
 import { Footer } from './components/Footer'
 
@@ -17,7 +16,6 @@ function App() {
       <ChakraProvider >
         <ApolloProvider client={client}>
           <div className="guru">
-            <Header />
             <Home />
             <Footer />
           </div>
