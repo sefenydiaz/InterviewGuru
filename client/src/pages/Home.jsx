8694b8ddce8c21e31cba4cde7e8f88912e84b99b
Lauren Miller <lmiller90@fordham.edu>
2023-07-31 08:47:34 -0400

    routed our root to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,45 +1,34 @@
-import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_MATCHUPS } from '../utils/queries';
+import { useState } from 'react';
+import Header from '../components/Header/index';
+import Form from './Resume';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
 
-const Home = () => {
-  const { loading, data } = useQuery(QUERY_MATCHUPS, {
-    fetchPolicy: "no-cache"
-  });
 
-  const matchupList = data?.matchups || [];
+export default function MainPage() {
+  const [currentPage, setCurrentPage] = useState('Form');
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+const handlePageChange = (page) => setCurrentPage(page);
 
-  return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+return (
+  <div>
+    {/* We are passing the currentPage from state and the function to update it */}
+    <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+    {/* Here we are calling the renderPage method which will return a component  */}
+    <main className="mx-3">{renderPage()}</main>
+  </div>
+);
 
-export default Home;
+}
