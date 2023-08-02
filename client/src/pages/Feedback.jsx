b6c1e1f36a2da0fddcdc47c6327df5c1980fc606
Dan G <daniel.w.gray1994@gmail.com>
2023-08-02 10:19:54 -0400

    added loading conditional to the question page

--- client/src/pages/Feedback.jsx
+++ client/src/pages/Feedback.jsx
@@ -5,55 +5,53 @@ import { Button } from "@chakra-ui/react";
 import { useGlobalData } from "../utils/GlobalDataContext";
 import { useNavigate } from "react-router-dom";
 
-const GET_FEEDBACK = gql `
-mutation GetFeedback($id: String!) {
-  getFeedback(_id: $id) {
-    _id
-    question
-    answer
-    industry
-    role
-    experience
-    feedback
+const GET_FEEDBACK = gql`
+  mutation GetFeedback($id: String!) {
+    getFeedback(_id: $id) {
+      _id
+      question
+      answer
+      industry
+      role
+      experience
+      feedback
+    }
   }
-}`
+`;
 const Feedback = () => {
-  const [getFeedback, { data, loading, error}] = useMutation(GET_FEEDBACK);
-  const navigate = useNavigate();
+  const [getFeedback, { data, loading, error }] = useMutation(GET_FEEDBACK);
 
   const { updateGlobalData, globalData } = useGlobalData();
 
-const showFeedback = async () => {
-  const { data } = await getFeedback({ 
-    variables: {
-      id: globalData._id
-    }
-  })
-  updateGlobalData(data.getFeedback)
-}
+  const showFeedback = async () => {
+    const { data } = await getFeedback({
+      variables: {
+        id: globalData._id,
+      },
+    });
+    updateGlobalData(data.getFeedback);
+  };
 
   useEffect(() => {
-  showFeedback()
+    showFeedback();
   }, []);
 
   if (loading) {
-    return <h2>Loading...</h2>
+    return <h2>Loading...</h2>;
   }
 
-
-return(
-  
-  <div>
-    <h2>Feedback:</h2>
-    <p>{globalData.feedback}</p>
-    <Button>
-      <Link to='/form'>Ask another question...</Link>
-    </Button>
-    <Button>
-      <Link to='/home'>Exit:</Link>
-    </Button>
-  </div>
-  )
+  return (
+    <div>
+      <h2>Feedback:</h2>
+      <p>{globalData.feedback}</p>
+      <Button>
+        <Link to="/form">Ask another question...</Link>
+      </Button>
+      <Button>
+        <Link to="/home">Exit:</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Feedback;
