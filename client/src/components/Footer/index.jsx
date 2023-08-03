cfcd539c9cd23788a013e44d206fcc4731d69278
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-08-03 15:14:53 +0300

    added simple footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,7 +1,28 @@
-
+import Auth from "../../utils/auth"
 
 const Footer = () => {
-  
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <footer className="w-100 mt-auto p-4" >
+        <div id="footer" className="container text-center mb-5">
+        <h4 id="dino">
+            Made with{' '}
+            <span
+              className="emoji"
+              role="img"
+              aria-label="dino"
+              aria-hidden="false"
+            >
+              🦕
+            </span>{' '}
+            
+          </h4>   
+        </div>
+    </footer>
+  )
 };
 
 export default Footer;
