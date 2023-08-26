f83571c6d16d010f39e5073677868bf16624e9aa
Dan G <daniel.w.gray1994@gmail.com>
2023-08-26 10:10:19 -0400

    added a login page test which checks the functionality of the login validators and POST request

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,25 +1,24 @@
 // INCLUDE NAVIGATION THAT INCLUDES: Stats, Home, Form(New Interview)
 
 import { Button, ButtonGroup, chakra } from "@chakra-ui/react";
-import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.png'
-import '../../assets/header.css'
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "../../assets/header.css";
 
 import Auth from "../../utils/auth";
 
-  const imgStyle = {
-    width: '450px',
-    height: 'auto',
-    borderRadius: '8px',
-  }
+const imgStyle = {
+  width: "450px",
+  height: "auto",
+  borderRadius: "8px",
+};
 
-  const HoverColorButton = chakra(Button, {
-    baseStyle: {
-      transition: 'background-color 0.3s ease',
-      _hover: { backgroundColor: '#96D1AD' }, 
-    },
-  });
-
+const HoverColorButton = chakra(Button, {
+  baseStyle: {
+    transition: "background-color 0.3s ease",
+    _hover: { backgroundColor: "#96D1AD" },
+  },
+});
 
 const Header = () => {
   const logout = (event) => {
@@ -27,7 +26,10 @@ const Header = () => {
     Auth.logout();
   };
   return (
-    <header id="header" className="bg-secondary text-light mb-4 py-3 flex-row align-center" >
+    <header
+      id="header"
+      className="bg-secondary text-light mb-4 py-3 flex-row align-center"
+    >
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/form">
@@ -42,7 +44,11 @@ const Header = () => {
             <>
               {/* <span>Hey there, {Auth.getProfile().data.username}!</span> */}
               <Link to="/login">
-                <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                <button
+                  data-test="logout-button"
+                  className="btn btn-lg btn-light m-2"
+                  onClick={logout}
+                >
                   Logout
                 </button>
               </Link>
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,9 +2,11 @@ describe("Login page", () => {
   beforeEach(() => {
     cy.visit("/login");
   });
-  it("Logs a pre-existing user in", () => {
+  it("Attempts login with correct email and incorrect password", () => {
+    // checks that Login header exists and implicitly confirms we are on the login page
     cy.getDataTest("login-form-header").should("contain.text", "Login");
 
+    // Correct email incorrect password scenario
     cy.getDataTest("email-input").within(() => {
       cy.get("label").should("have.text", "Email: ");
       cy.get("input").type("stjimmy400@gmail.com");
@@ -12,11 +14,48 @@ describe("Login page", () => {
 
     cy.getDataTest("password-input").within(() => {
       cy.get("label").should("have.text", "Password: ");
+      cy.get("input").type("qswdfjwnef");
+    });
+
+    cy.getDataTest("login-button").click();
+    cy.location("pathname").should("equal", "/login");
+  });
+
+  it("Attempts login with correct email and incorrect password", () => {
+    // Incorrect email correct password scenario
+    cy.getDataTest("email-input").within(() => {
+      cy.get("input").type("jimmy400@gmail.com");
+    });
+
+    cy.getDataTest("password-input").within(() => {
+      cy.get("input").type("Serum54321!");
+    });
+
+    cy.getDataTest("login-button").click();
+    cy.location("pathname").should("equal", "/login");
+  });
+
+  it("Logs a pre-existing user in and logs the user out", () => {
+    // Correct email/password scenario
+    cy.getDataTest("email-input").within(() => {
+      cy.get("input").type("stjimmy400@gmail.com");
+    });
+
+    cy.getDataTest("password-input").within(() => {
       cy.get("input").type("Serum54321!");
     });
 
     cy.getDataTest("login-button").should("have.text", "Login!");
+
+    cy.intercept("POST", "http://localhost:3000/login", {
+      fixture: "successful-login.json",
+    });
     cy.getDataTest("login-button").click();
     cy.location("pathname").should("equal", "/home");
+
+    // logout test
+    cy.getDataTest("logout-button").should("have.text", "Logout");
+    cy.getDataTest("logout-button").click();
+    cy.location("pathname").should("equal", "/login");
   });
 });
