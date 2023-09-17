f7852f1c1e6bef9a0626d13e64ec694a05163a22
Dan G <daniel.w.gray1994@gmail.com>
2023-09-17 11:15:58 -0400

    added a modal that prompts the user to confirm when 'delete account' button is clicked

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -2,10 +2,46 @@
 // access past interviews + feedback
 // INCLUDE IN NAVIGATION
 
-import { Button } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const Stats = () => {
-  return <Button colorScheme="red">Delete Account</Button>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen} colorScheme="red">
+        Delete Account
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            This is a permanent action. Are you sure you want to delete your
+            account permanently?
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose} mr={3}>
+              Cancel
+            </Button>
+            <Button colorScheme="red">Delete Account</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default Stats;
