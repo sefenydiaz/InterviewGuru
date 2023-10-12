83d24ab91123e1e542428e73b49dcf32c2413f8f
Dan G <daniel.w.gray1994@gmail.com>
2023-10-12 12:06:49 -0400

    added a config file and ran the first test

--- test/spec/login.spec.js
+++ test/spec/login.spec.js
@@ -1,12 +1,16 @@
 const { By, Builder } = require("selenium-webdriver");
 const assert = require("assert");
 
-const loginTest = async () => {
+describe("Login/Logout Test", () => {
   let driver;
+  const baseURL = "http://localhost:3000/";
 
-  try {
+  before(async () => {
     driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://localhost:3000/");
+  });
+
+  it("Should log a user in and then out", async () => {
+    await driver.get(baseURL);
 
     let title = await driver.getTitle();
     assert.equal("Interview Guru", title);
@@ -16,9 +20,16 @@ const loginTest = async () => {
     const emailTextBox = await driver.findElement(By.id("email-input"));
     const pwordTextBox = await driver.findElement(By.id("pword-input"));
     const loginButton = await driver.findElement(By.id("login-button"));
-  } catch (error) {
-    console.log(error);
-  } finally {
+
+    emailTextBox.sendKeys("stjimmy400@gmail.com");
+    pwordTextBox.sendKeys("Serum54321!");
+    loginButton.click();
+
+    let URL = await driver.getCurrentUrl();
+    assert.equal(baseURL, URL);
+  });
+
+  after(async () => {
     await driver.quit();
-  }
-};
+  });
+});
