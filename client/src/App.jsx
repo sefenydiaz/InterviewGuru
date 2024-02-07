c9e7ccfc59db26f70b3ccc7dc2643cdbe5fb01ce
Dan G <daniel.w.gray1994@gmail.com>
2024-02-07 13:23:30 -0500

    attempted favicon fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,32 @@
-import './App.css';
-
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { ChakraProvider } from '@chakra-ui/react'; 
-import Header  from './components/Header/index'
-import Footer from './components/Footer/index'
-import { setContext } from '@apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
-import { InterviewProvider } from './utils/InterviewContext';
-import { GlobalDataProvider } from './utils/GlobalDataContext'
+import "./App.css";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+import { InterviewProvider } from "./utils/InterviewContext";
+import { GlobalDataProvider } from "./utils/GlobalDataContext";
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
-
-
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -35,26 +37,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
 //put in user state to outlet using props
 function App() {
-  
   return (
     <InterviewProvider>
-      <ChakraProvider >
+      <ChakraProvider>
         <ApolloProvider client={client}>
-          <GlobalDataProvider >
-          <div className="container">
-            <Header />
-
+          <GlobalDataProvider>
             <div className="container">
-            <Outlet 
-            />
+              <Header />
+
+              <div className="container">
+                <Outlet />
+              </div>
+
+              <Footer />
             </div>
-
-
-            <Footer />
-          </div>
           </GlobalDataProvider>
         </ApolloProvider>
       </ChakraProvider>
